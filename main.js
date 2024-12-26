aa8f80363798dd3cd97cd8e88b4b45c3bad45457
shorouk <shorouktaha19121998@gmail.com>
2024-12-26 21:01:55 +0200

    add some changes

--- main.js
+++ main.js
@@ -1,24 +1,30 @@
-document.getElementById("menu-icon").addEventListener("click", function () {
-    var navList = document.getElementById("nav-list");
+
+document.addEventListener("DOMContentLoaded", () => {
+  const menuIcon = document.querySelector("#menu-icon");
+  const navList = document.querySelector("#nav-list");
+
+  // Toggle visibility of the menu
+  menuIcon.addEventListener("click", () => {
     navList.classList.toggle("active");
+  });
 });
 let currentIndex = 0;
 const house = document.querySelectorAll(".houses .item1");
 const totalHouses = house.length;
 
 document.getElementById("next").addEventListener("click", () => {
-    currentIndex = (currentIndex + 1) % totalHouses;
-    updateSlider();
+  currentIndex = (currentIndex + 1) % totalHouses;
+  updateSlider();
 });
 
 document.getElementById("prev").addEventListener("click", () => {
-    currentIndex = (currentIndex - 1 + totalHouses) % totalHouses;
-    updateSlider();
+  currentIndex = (currentIndex - 1 + totalHouses) % totalHouses;
+  updateSlider();
 });
 
 function updateSlider() {
   const offset = -currentIndex * 100;
-    house.forEach((house) => {
+  house.forEach((house) => {
     house.style.transform = `translateX(${offset}%)`;
-    });
+  });
 }
